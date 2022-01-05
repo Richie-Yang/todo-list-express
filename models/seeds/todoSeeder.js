10e66e3e51184036171d0f3125d4bc495deb0931
Richie-Yang <richieyang8813@gmail.com>
2022-01-05 16:34:12 +0800

    refactor: modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,41 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const Todo = require('../todo')
+const User = require('../user')
 const db = require('../../config/mongoose')
 
-db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
 
-  console.log('done')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@root.com',
+  password: '123',
+}
+
+
+db.once('open', () => {
+  return bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => {
+      return User.create({ 
+        name: SEED_USER.name, 
+        email: SEED_USER.email, 
+        password: hash 
+      })
+    })
+    .then(user => {
+      const userId = user._id
+
+      return Promise.all(Array.from({ length: 10 }, (_, i) => {
+        return Todo.create({ name: `name-${i}`, userId })
+      }))
+    })
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })

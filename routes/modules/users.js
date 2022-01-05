81ccf592dd19bf10dad6a72836557dfe305f2f13
Richie-Yang <richieyang8813@gmail.com>
2022-01-05 09:09:46 +0800

    feat: add bcryptjs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 const User = require('../../models/user')
 
@@ -40,11 +41,15 @@ router.post('/register', (req, res) => {
         res.render('register', {
           errors, name, email, password, confirmPassword
         })
-      } else {
-        return User.create({ name, email, password })
-          .then(() => res.redirect('/'))
-          .catch(err => console.log(err))
-      }
+      } 
+      return bcrypt.genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+        .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+        .then(hash => {
+          // 用雜湊值取代原本的使用者密碼
+          return User.create({ name, email, password: hash }) 
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+        })
     })
 })
 

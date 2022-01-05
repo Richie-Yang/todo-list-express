6ceee94190a5c0e9f2d1176438718b845de78a3d
Richie-Yang <richieyang8813@gmail.com>
2022-01-05 15:02:14 +0800

    feat: add facebook login & dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
+const facebookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user')
 
 
@@ -31,6 +32,30 @@ module.exports = app => {
         .catch(err => done(err, false))
     }
   ))
+
+  passport.use(new facebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+    const { email, name } = profile._json
+
+    return User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+
+        const randomPassword = Math.random().toString(36).slice(-8)
+        return bcrypt.genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => {
+            return User.create({ name, email, password: hash })
+          })
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }))
 
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
